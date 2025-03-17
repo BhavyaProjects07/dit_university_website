ff99908c3bbba342300c35f476492947ee4ce779
BhavyaProjects07 <laptopuse01824x@gmail.com>
2025-03-17 18:19:25 +0530

    added psycopg

--- scripts.py
+++ scripts.py
@@ -5,3 +5,8 @@ model = genai.GenerativeModel('gemini-2.0-flash')
 
 response = model.generate_content("Create a simple html css code template")
 print(response.text)
+
+
+# git command to push every change 
+
+#  --->   git add . ; git commit -m "Databse change" ; git push origin main
